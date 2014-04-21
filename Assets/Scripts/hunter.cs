a23cbe90785c16488d8a985d4f8daa31f9540af4
Joseph Utecht <thenmal@gmail.com>
2014-04-20 19:44:55 -0500

    hunter working sans mana

--- Assets/Scripts/hunter.cs
+++ Assets/Scripts/hunter.cs
@@ -2,21 +2,23 @@
 using System.Collections;
 
 public class hunter : MonoBehaviour {
-	public GameObject sword;
-	public float sword_reswing;
-	private float next_swing;
+	public GameObject arrow;
+	public float shot_speed;
+	public float multi_shot_speed;
+	private float next_shot;
+	private float next_multishot;
 	private Camera camera;
 	private PlayerControl pc;
 
 	// Use this for initialization
 	void Start () {
-		next_swing = Time.time;
+		next_shot = Time.time;
+		next_multishot = Time.time;
 		pc = gameObject.GetComponent<PlayerControl>();
 		camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 	}
 
-	void SwingSword(){
-		Quaternion q = Quaternion.Euler(0f, 0f, pc.dir);
+	void Multishot(){
 		Vector3 place = transform.position;
 		Vector3 offset = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.extents;
 		if(pc.dir == 0){
@@ -28,24 +30,50 @@ public class hunter : MonoBehaviour {
 		} else if(pc.dir == 270){
 			place.y = place.y - offset.y;
 		}
-		Network.Instantiate(sword, place, q, 0);
+		float new_dir = Random.Range(-15, 15) + pc.dir;
+		Quaternion q = Quaternion.Euler(0f, 0f, new_dir);
+		Network.Instantiate(arrow, place, q, 0);
 		//g.GetComponent<shot>().player = gameObject;
 	}
 
-	void ThreateningShout(){
+	void ShootArrow(){
+		Vector3 place = transform.position;
+		Network.Instantiate(arrow, place, TowardsClosestTarget(), 0);
+	}
 
+	Quaternion TowardsClosestTarget(){
+		float closest_distance = 999f;
+		GameObject closest_enemy = null;
+		foreach(GameObject go in GameObject.FindGameObjectsWithTag("enemy")){
+			float dist = Vector3.Distance(go.transform.position, transform.position);
+			if(dist < closest_distance){
+				closest_enemy = go;
+				closest_distance = dist;
+			}
+		}
+		if(closest_enemy == null){
+			return Quaternion.Euler(0f, 0f, pc.dir);
+		} else {
+			// How you get an angle towards a target in 2D
+			Vector3 target = closest_enemy.transform.position - transform.position;
+			target.Normalize();
+			return Quaternion.AngleAxis(Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg, Vector3.forward);
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButton("A")){
-			if(Time.time > next_swing){
-				next_swing = sword_reswing + Time.time;
-				SwingSword();
+			if(Time.time > next_shot){
+				next_shot = shot_speed + Time.time;
+				ShootArrow();
 			}
 		}
 		if(Input.GetButton("B")){
-			ThreateningShout();
+			if(Time.time > next_multishot){
+				Multishot();
+				next_multishot = multi_shot_speed + Time.time;
+			}
 		}
 	}
 }
